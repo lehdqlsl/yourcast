336fab1941c91487335974986fc17078d73015ca
lehdqlsl <lehdqlsl@naver.com>
2017-09-28 09:27:22 +0900

    방송 메인 리스트

--- src/main/java/com/yourcast/app/BroadcastController.java
+++ src/main/java/com/yourcast/app/BroadcastController.java
@@ -2,19 +2,25 @@ package com.yourcast.app;
 
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.yourcast.app.service.BroadcastService;
+
 @Controller
 public class BroadcastController {
+	@Autowired
+	private BroadcastService bservice;
 
 	@RequestMapping(value = "/bs/{id}", method = RequestMethod.GET)
-	public String home(@PathVariable(value="id") String id, Model model) {
-		String url = "rtmp://192.168.0.31:3535/myapp/"+id;
-		model.addAttribute("url",url);
+	public String home(@PathVariable(value = "id") String id, Model model) {
+		String stream_key = bservice.getInfo(Integer.parseInt(id)).getStream_key();
+		String url = "rtmp://192.168.0.31:3535/myapp/" + stream_key;
+		model.addAttribute("url", url);
 		return ".broadcast";
 	}
 }
--- src/main/java/com/yourcast/app/HomeController.java
+++ src/main/java/com/yourcast/app/HomeController.java
@@ -10,9 +10,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.yourcast.app.service.BroadcastService;
 import com.yourcast.app.service.CategoryService;
 import com.yourcast.app.service.MemberProfileService;
 import com.yourcast.app.service.MemberService;
+import com.yourcast.app.vo.BroadcastVO;
 import com.yourcast.app.vo.CategoryVO;
 import com.yourcast.app.vo.MemberProfileVO;
 import com.yourcast.app.vo.MemberVO;
@@ -25,19 +27,27 @@ public class HomeController {
 
 	@Autowired
 	private CategoryService service;
-	
+
 	@Autowired
 	private MemberService m_sevice;
-	
+
 	@Autowired
 	private MemberProfileService mp_service;
 
+	@Autowired
+	private BroadcastService b_service;
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
+		List<BroadcastVO> blist = b_service.getList();
+		int cnt = (int) Math.ceil(blist.size() / 4.0);
+
+		model.addAttribute("blist", blist);
+		model.addAttribute("cnt", cnt);
+		model.addAttribute("end", blist.size()-1);
+
 		return ".main";
 	}
-
-
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public String personnel(@PathVariable(value = "id") String id, Model model) {
@@ -46,7 +56,7 @@ public class HomeController {
 		MemberProfileVO voMP = mp_service.getInfo(vo.getM_num());
 		model.addAttribute("clist", clist);
 		model.addAttribute("id", id);
-		model.addAttribute("voMP",voMP);
+		model.addAttribute("voMP", voMP);
 		return ".personnel";
 	}
 }
